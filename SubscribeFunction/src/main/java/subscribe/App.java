f595fd601eb230d21d784ee5434ec2575e77f99c
illusi0n <milosbiljanovic@gmail.com>
2020-01-06 17:39:51 +0100

    Fixed returning proper response. No more 502.

--- SubscribeFunction/src/main/java/subscribe/App.java
+++ SubscribeFunction/src/main/java/subscribe/App.java
@@ -3,21 +3,24 @@ package subscribe;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpStatus;
 import subscribe.dto.UserInfoDto;
+import subscribe.response_messages.EmailAlreadyExists;
+import subscribe.response_messages.InvalidUserSubscription;
+import subscribe.response_messages.ParsingUserSubscriptionFailed;
+import subscribe.response_messages.ResponseMessage;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
  * Handler for requests to Lambda function.
  */
-public class App implements RequestHandler<InputStream, GatewayResponse> {
+public class App implements RequestHandler<Object, Object> {
 
     private DynamoDbWrapper dynamoDbWrapper;
 
@@ -25,22 +28,33 @@ public class App implements RequestHandler<InputStream, GatewayResponse> {
         this.dynamoDbWrapper = new DynamoDbWrapper();
     }
 
-    public GatewayResponse handleRequest(final InputStream lambdaInput, final Context context) {
+    public Object handleRequest(final Object lambdaInput, final Context context) {
         UserInfoDto userSubscription = this.getUserSubscriptionDto(lambdaInput);
-        validateInput(userSubscription);
+        if (userSubscription == null) {
+            ParsingUserSubscriptionFailed parsingUserSubscriptionFailedResponse = new ParsingUserSubscriptionFailed();
+            System.out.println(parsingUserSubscriptionFailedResponse);
+            return this.createResponseBadRequest(parsingUserSubscriptionFailedResponse);
+        }
+
+        boolean isValidInput = validateInput(userSubscription);
+        if (!isValidInput) {
+            InvalidUserSubscription invalidUserSubscriptionResponse = new InvalidUserSubscription();
+            System.out.println(invalidUserSubscriptionResponse);
+            return this.createResponseBadRequest(invalidUserSubscriptionResponse);
+        }
+
         if (dynamoDbWrapper.userSubscriptionExists(userSubscription)) {
-            throw new RuntimeException("Email already exists.");
+            EmailAlreadyExists emailAlreadyExistsResponse = new EmailAlreadyExists();
+            System.out.println(emailAlreadyExistsResponse);
+            return this.createResponseBadRequest(emailAlreadyExistsResponse);
         }
 
         dynamoDbWrapper.createUserSubscription(userSubscription);
         return createResponseSuccessfullySubscribed(userSubscription);
     }
 
-    private void validateInput(UserInfoDto userSubscription) {
-        boolean isValid = userSubscription != null && isValidEmail(userSubscription.getEmail());
-        if (!isValid) {
-            throw new RuntimeException("Invalid payload, don't go down to AWS to waste money and free tier quota");
-        }
+    private boolean validateInput(UserInfoDto userSubscription) {
+        return userSubscription != null && isValidEmail(userSubscription.getEmail());
     }
 
     private static boolean isValidEmail(String email) {
@@ -50,24 +64,29 @@ public class App implements RequestHandler<InputStream, GatewayResponse> {
     }
 
     private GatewayResponse createResponseSuccessfullySubscribed(UserInfoDto userSubscription) {
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
-        ObjectMapper jsonConverter = new ObjectMapper();
         try {
+            ObjectMapper jsonConverter = new ObjectMapper();
             String jsonUserSubscription = jsonConverter.writeValueAsString(userSubscription);
-            return new GatewayResponse(jsonUserSubscription, headers, HttpStatus.SC_CREATED);
+            return createJsonResponse(jsonUserSubscription, HttpStatus.SC_CREATED);
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
             throw new RuntimeException("Could not create response object from user subscription.");
         }
     }
 
-    private UserInfoDto getUserSubscriptionDto(InputStream lambdaInput) {
+    private UserInfoDto getUserSubscriptionDto(Object lambdaInput) {
         UserInfoDto userSubscription = null;
+        if (!(lambdaInput instanceof LinkedHashMap)) {
+            return null;
+        }
+
+        LinkedHashMap<String, String> lambdaMapInput = (LinkedHashMap<String, String>) lambdaInput;
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            JsonNode jsonNode = objectMapper.readTree(lambdaInput);
-            String body = jsonNode.get("body").asText();
+            if (lambdaMapInput.get("body") == null) {
+                return null;
+            }
+
+            String body = lambdaMapInput.get("body");
             body = java.net.URLDecoder.decode(body, StandardCharsets.UTF_8.name());
             userSubscription = objectMapper.readValue(body, UserInfoDto.class);
         } catch (IOException e) {
@@ -77,4 +96,26 @@ public class App implements RequestHandler<InputStream, GatewayResponse> {
         return userSubscription;
     }
 
+    private GatewayResponse createResponseInternalError() {
+        return createJsonResponse("{}", HttpStatus.SC_INTERNAL_SERVER_ERROR);
+    }
+
+
+    private GatewayResponse createResponseBadRequest(ResponseMessage message) {
+        try {
+            ObjectMapper jsonConverter = new ObjectMapper();
+            String jsonUserSubscription = jsonConverter.writeValueAsString(message);
+            return createJsonResponse(jsonUserSubscription, HttpStatus.SC_BAD_REQUEST);
+        } catch (JsonProcessingException e) {
+            System.out.println("Could not create response object from user subscription.");
+            return createResponseInternalError();
+        }
+    }
+
+    private GatewayResponse createJsonResponse(String jsonBody, int statusCode) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+        return new GatewayResponse(jsonBody, headers, statusCode);
+    }
 }
---
+++ SubscribeFunction/src/main/java/subscribe/response_messages/EmailAlreadyExists.java
@@ -0,0 +1,7 @@
+package subscribe.response_messages;
+
+public class EmailAlreadyExists extends ResponseMessage {
+    public EmailAlreadyExists() {
+        super("Email already exists.");
+    }
+}
